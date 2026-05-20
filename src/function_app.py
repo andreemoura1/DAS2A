d3b2e7abfe0a69afaefa77692ae6c51a6738da55
GUILHERME FÜCHTER DOS SANTOS <guilherme.fuchter@univille.br>
2026-05-20 20:24:46 -0300

    alteração extract ++

--- src/function_app.py
+++ src/function_app.py
@@ -9,8 +9,17 @@ app = func.FunctionApp()
 from triggers.extract_cliente import app as extract_cliente
 from triggers.extract_pedido import app as extract_pedido
 from triggers.extract_entrega import app as extract_entrega
+from triggers.extract_produto import app as extract_produto
+from triggers.extract_categoria_produto import app as extract_categoria_produto
+from triggers.extract_estoque_movimentacao import app as extract_estoque_movimentacao
+from triggers.extract_regiao import app as extract_regiao
+
 
 # Registrar as azure functions
 app.register_functions(extract_cliente)
 app.register_functions(extract_pedido)
 app.register_functions(extract_entrega)
+app.register_functions(extract_produto)
+app.register_functions(extract_categoria_produto)
+app.register_functions(extract_estoque_movimentacao)
+app.register_functions(extract_regiao)
