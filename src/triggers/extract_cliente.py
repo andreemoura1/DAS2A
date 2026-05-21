848066435561ffad256c80dc848176f4dbcdd29f
GUILHERME FÜCHTER DOS SANTOS <guilherme.fuchter@univille.br>
2026-05-21 20:27:26 -0300

    alteração

--- src/triggers/extract_cliente.py
+++ src/triggers/extract_cliente.py
@@ -14,10 +14,5 @@ def extract_cliente(timer: func.TimerRequest) -> None:
     sql_user = os.getenv("SQL_USER_SOURCE")
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
- 	logging.info(f"servidor: {sql_server},  banco: {sql_database}, usuario:{sql_user}, senha: {sql_pass} ...")   
-    """
-    Trigger de extração agendada (diária às 06:00 UTC).
-    Apenas delega para o orchestrator — sem lógica de negócio aqui.
-    """
-    logging.info("extract_cliente iniciado.")
-    logging.info("extract_cliente finalizado.")
+    logging.info(f"servidor: {sql_server},  banco: {sql_database}, usuario:{sql_user}, senha: {sql_pass} ...")   
+  
